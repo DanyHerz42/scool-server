1eae750a1a037a2ff1d0587c9e91fed054a3d29c
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-08 16:46:02 -0500

    GETÂ complete info student

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,4 +1,6 @@
 const query = require('../db/mysql.conn');
+const { get } = require('../routes/users.routes');
+
 exports.completeInfo = async(req, res) => {
   const {biography, image, name_org} = req.body;
   const {id_user} = req.userFound[0];
@@ -19,5 +21,16 @@ exports.completeInfo = async(req, res) => {
     res.status(200).json({message:"Info completada con exito"})
   } catch (error) {
     res.status(500).json({error})
+  }
+}
+
+exports.getProfile = async(req, res) => {
+  let {userFound} = req;
+  try {
+    const getProfile = await query(`SELECT * FROM students JOIN organizations ON organizations.id_org = students.id_org WHERE ?`, {id_user: req.userFound[0].id_user})
+    getProfile.unshift(userFound)
+    res.status(200).json({message: "Esta es la info del usuario", getProfile})
+  } catch (error) {
+    res.status(500).json({message: "Algo ha fallado", error})
   }
 }
--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
 
-const {completeInfo} = require('../controllers/students.controller')
+const {completeInfo, getProfile} = require('../controllers/students.controller')
 
-
+router.get("/get-profile", validarJWT, getProfile)
 router.post("/complete-info", validarJWT ,completeInfo);
 
 module.exports = router;
