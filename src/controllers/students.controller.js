a9e9dfad8c7b1ecfb456f80002ecf1da444c9780
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-12 15:10:55 -0500

    New WS

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -34,3 +34,33 @@ exports.getProfile = async(req, res) => {
     res.status(500).json({message: "Algo ha fallado", error})
   }
 }
+
+exports.registerInClass = async(req, res) => {
+  const {unique_identifier} = req.body;
+  console.log(req.userFound);
+  try {
+    const getIdStudent = await query(`SELECT id_student FROM students WHERE id_user = ${req.userFound[0].id_user}`)
+    const verifyClass = await query(`SELECT id_class FROM classes WHERE unique_identifier = "${unique_identifier}";`)
+    if(verifyClass < 1){
+      return res.status(401).json({message: "Esta clase no existe"})
+    }
+    const addClass = await query(`INSERT INTO relation_students_classes SET ?`, {id_class: verifyClass[0].id_class, id_student: getIdStudent[0].id_student, id_status: 1})
+    res.status(200).json({message: "Alumno registrado exitosamente"})
+  } catch (error) {
+    res.status(500).json({message: "Algo ha fallado", error})
+  }
+}
+
+exports.getClasses = async(req, res) => {
+  try {
+    const getIdStudent = await query(`SELECT id_student FROM students WHERE id_user = ${req.userFound[0].id_user}`)
+    const classes = await query(`SELECT classes.name, classes.description, classes.color, teachers.id_teacher, users.name_user AS teacher_username FROM students 
+                                JOIN relation_students_classes ON students.id_student = relation_students_classes.id_student
+                                JOIN classes ON relation_students_classes.id_class = classes.id_class
+                                JOIN teachers ON teachers.id_teacher = classes.id_teacher
+                                JOIN users ON teachers.id_user = users.id_user WHERE students.id_student = ${getIdStudent[0].id_student};`);
+    res.status(200).json({ok: true, message: "Estas son las classes de este alumno", classes})
+  } catch (error) {
+    res.status(500).json({ok: false, message: "Algo ha fallado", error})
+  }
+}
--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
 
-const {completeInfo, getProfile} = require('../controllers/students.controller')
+const {completeInfo, getProfile, registerInClass, getClasses} = require('../controllers/students.controller')
 
-router.get("/get-profile", validarJWT, getProfile)
-router.post("/complete-info", validarJWT ,completeInfo);
-
+router.get("/get-profile", validarJWT, getProfile);
+router.post("/complete-info", validarJWT, completeInfo);
+router.post("/register-in-class", validarJWT, registerInClass);
+router.get("/get-student-classes", validarJWT, getClasses)
 module.exports = router;
