38fcd4ec845dbfd325958a2a675bba4a57c60e68
Daniel Hernandez <dhernandez30@ucol.mx>
2021-10-18 17:07:09 -0500

    WS teacher profile

--- src/routes/deliverys.routes.js
+++ src/routes/deliverys.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { deliveryHomework } = require('../controllers/deliverys.controller');
 const router = express.Router();
 const { validarJWT } = require('../middlewares/validar-jwt');
 
-router.post("/delivery-homework", validarJWT)
+
+
+router.post("/delivery-homework", validarJWT, deliveryHomework);
